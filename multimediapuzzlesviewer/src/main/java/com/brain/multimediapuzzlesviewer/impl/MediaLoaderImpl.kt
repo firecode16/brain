458ec54cbf467ff2bbd8a3482731fda8064d6fd8
FireCode <69737708+firecode16@users.noreply.github.com>
2024-05-06 19:17:17 -0600

    firecode16
    delete 3 impl

--- multimediapuzzlesviewer/src/main/java/com/brain/multimediapuzzlesviewer/impl/MediaLoaderImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.brain.multimediapuzzlesviewer.impl
-
-import android.widget.ImageView
-
-/*
-interface MediaLoaderImpl<T> {
-    fun loadMedia(imageView: ImageView?, objMedia: T?)
-}*/
--- multimediapuzzlesviewer/src/main/java/com/brain/multimediapuzzlesviewer/impl/OnMediaChangeListenerImpl.kt
+++
@@ -1,5 +0,0 @@
-package com.brain.multimediapuzzlesviewer.impl
-
-interface OnMediaChangeListenerImpl {
-    fun onMediaChange(position: Int)
-}
